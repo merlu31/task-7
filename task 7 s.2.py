58bc7414be08bd73086ea691c784cf0e5a85ab84
merlu31 <160389247+merlu31@users.noreply.github.com>
2024-03-26 18:50:53 +0530

    Add files via upload

---
+++ task 7 s.2.py
@@ -0,0 +1,8 @@
+## write another python program to read from textfile .
+#the function will take the name of the file and display the content of file to console.
+file =open(r"C:\Users\Merlin Archana\OneDrive\Desktop\task 7/python.txt", "w")
+file.write("26032024")
+file =open(r"C:\Users\Merlin Archana\OneDrive\Desktop\task 7/python.txt", "r")
+file.read()
+print(260324)
+file.close()
